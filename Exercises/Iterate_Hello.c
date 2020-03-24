d9aa2efb0b7b1dcaa469b4ed55b6df1b64d11c08
Sam Fieldman <Sam.A.Fieldman@gmail.com>
2020-03-23 22:51:43 -0400

    Bug for process with rank N-1 fixed.

--- Exercises/Iterate_Hello.c
+++ Exercises/Iterate_Hello.c
@@ -18,7 +18,7 @@
 int main ( int argc, char *argv[] )
 {
         MPI_Init(&argc, &argv);
-        MPI_Comm mpi_0 = MPI_COMM_WORLD;
+        MPI_Comm mcw_0 = MPI_COMM_WORLD;
 
         int rank;
         int size;
@@ -31,18 +31,25 @@ int main ( int argc, char *argv[] )
 	
 	while (iter < 3) {
 		if (rank == 0) {
-			printf("Hello from Rank %d. I recieved data from rank %d. Iteration %d.", rank, data, iter);
+			if (iter != 0) {
+				MPI_Recv(&data, 1, MPI_INT, size - 1, 0, mcw_0, MPI_STATUS_IGNORE);	
+			}	
+			printf("Hello from Rank %d. I recieved data from rank %d. Iteration %d.\n", rank, data, iter);
 			MPI_Send(&rank, 1, MPI_INT, rank + 1, 0, mcw_0);
-			MPI_Recv(&data, 1, MPI_INT, size - 1, 0, mcw_0);
+		}
+		else if (rank < size -1) {
+			MPI_Recv(&data, 1, MPI_INT, rank - 1, 0, mcw_0, MPI_STATUS_IGNORE);
+			printf("Hello from Rank %d. I recieved data from rank %d. Iteration %d.\n", rank, data, iter);
+                        MPI_Send(&rank, 1, MPI_INT, rank + 1 , 0, mcw_0);
 		}
 		else {
-			MPI_Recv(&data, 1, MPI_INT, rank - 1, 0, mcw_0);
-			printf("Hello from Rank %d. I recieved data from rank %d. Iteration %d.", rank, data, iter);
-                        MPI_Send(&rank, 1, MPI_INT, rank + 1 % size, 0, mcw_0);
+			MPI_Recv(&data, 1, MPI_INT, rank - 1, 0, mcw_0, MPI_STATUS_IGNORE);
+                        printf("Hello from Rank %d. I recieved data from rank %d. Iteration %d.\n", rank, data, iter);
+                        MPI_Send(&rank, 1, MPI_INT, 0, 0, mcw_0);
 		}
 		iter++;
 	}
-	MPI_Barrier(mcw_0);
+
         MPI_Finalize();
 
         return 0;
