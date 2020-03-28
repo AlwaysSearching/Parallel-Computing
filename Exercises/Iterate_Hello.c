b7666545d95a02d0cf63778c7dd094833386d4c2
Sam Fieldman <Sam.A.Fieldman@gmail.com>
2020-03-27 20:10:48 -0400

    comments added.

--- Exercises/Iterate_Hello.c
+++ Exercises/Iterate_Hello.c
@@ -22,7 +22,8 @@ int main ( int argc, char *argv[] )
 
         int rank;
         int size;
-
+	
+	// Get rank in group and number of processes.
         MPI_Comm_rank(mcw_0, &rank);
         MPI_Comm_size(mcw_0, &size);
 	
@@ -31,18 +32,19 @@ int main ( int argc, char *argv[] )
 	
 	while (iter < 3) {
 		if (rank == 0) {
-			if (iter != 0) {
-				MPI_Recv(&data, 1, MPI_INT, size - 1, 0, mcw_0, MPI_STATUS_IGNORE);	
-			}	
+			// Print status. Send message to following process. Wait on message from process size - 1.
 			printf("Hello from Rank %d. I recieved data from rank %d. Iteration %d.\n", rank, data, iter);
 			MPI_Send(&rank, 1, MPI_INT, rank + 1, 0, mcw_0);
+			MPI_Recv(&data, 1, MPI_INT, size - 1, 0, mcw_0, MPI_STATUS_IGNORE);
 		}
 		else if (rank < size -1) {
+			// Wait to recieve message from prior process. Print Status. Send to following process.
 			MPI_Recv(&data, 1, MPI_INT, rank - 1, 0, mcw_0, MPI_STATUS_IGNORE);
 			printf("Hello from Rank %d. I recieved data from rank %d. Iteration %d.\n", rank, data, iter);
                         MPI_Send(&rank, 1, MPI_INT, rank + 1 , 0, mcw_0);
 		}
 		else {
+			// Wait for message. Print status. Send message to process 0.
 			MPI_Recv(&data, 1, MPI_INT, rank - 1, 0, mcw_0, MPI_STATUS_IGNORE);
                         printf("Hello from Rank %d. I recieved data from rank %d. Iteration %d.\n", rank, data, iter);
                         MPI_Send(&rank, 1, MPI_INT, 0, 0, mcw_0);
