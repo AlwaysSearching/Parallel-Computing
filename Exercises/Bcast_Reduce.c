fe70dde04c704d603a3f7ec6208d962ef0676de4
Sam Fieldman <Sam.A.Fieldman@gmail.com>
2020-03-28 17:41:14 -0400

    User sets the seed. Prints proper usage if incorrect params passed.

--- Exercises/Bcast_Reduce.c
+++ Exercises/Bcast_Reduce.c
@@ -8,12 +8,20 @@
  *        Created:  3/28/2020  
  *
  *         Author: Sam Fieldman 
- *
+ *	
+ *  Purpuse/usage: The program takes in two arguments from the command line/ The first
+ *  		   is an integer value that represents the maximum possible value the
+ *  		   random number, N, can take. The second is the seed to pass to 
+ *  		   srand so that different results can be orbtained. 
+ *  		   (rand is always seeded to one.) 
+ *		
  * =====================================================================================
  */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <mpi.h>
+
 
 int main ( int argc, char *argv[] )
 {
@@ -23,14 +31,27 @@ int main ( int argc, char *argv[] )
         int rank;
         int size;
 
-        MPI_Comm_rank(mcw_0, &rank);
-        MPI_Comm_size(mcw_0, &size);
+        MPI_Comm_rank(mpi_0, &rank);
+        MPI_Comm_size(mpi_0, &size);
+
+	// simple check to ensure that we have 2 args.
+	if (argc != 3) {
+		if (rank == 0) {
+			printf("Usage: Bcast_Reduce <N> <seed> \n");
+			printf("N: Random int in range (0, N) \n");
+			printf("seed: Seed for drawing random integers. \n");
+		}
+		MPI_Finalize();
+		return 0;
+	}
 
 	// the value of N will be a random value between N and the value passed in argv
 	int N;
 
 	if (rank == 0) {
 		int limit = atoi(argv[1]);
+		int seed = atoi(argv[2]);
+		srand(seed);
 		N = rand() % limit;
 	}
 	
@@ -41,11 +62,11 @@ int main ( int argc, char *argv[] )
 	N = N - rank;
 
 	// Use Reduce to find the sum, min, and max
-	int min, max. sum;
+	int min, max, sum;
 
-	MPI_Reduce(&N, &sum, 1, MPI_INT, MPI_SUM, mpi_0);
-	MPI_Reduce(&N, &max, 1, MPI_INT, MPI_MAX, mpi_0);
-	MPI_Reduce(&N, &min, 1, MPI_INT, MPI_MIN, mpi_0);
+	MPI_Reduce(&N, &sum, 1, MPI_INT, MPI_SUM, 0, mpi_0);
+	MPI_Reduce(&N, &max, 1, MPI_INT, MPI_MAX, 0, mpi_0);
+	MPI_Reduce(&N, &min, 1, MPI_INT, MPI_MIN, 0, mpi_0);
 
 	// process 0 prints the results.
 	if (rank == 0) {
@@ -55,7 +76,7 @@ int main ( int argc, char *argv[] )
 		printf("The max is: %d \n", max);
 	}
 
-        MPI_Finalize()
+        MPI_Finalize();
         return 0;
 }
 
